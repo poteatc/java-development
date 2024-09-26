1dbe2bb2deece2f886bcb49011d33ef8f79c89c0
poteatc <poteatc@outlook.com>
2024-09-26 09:57:13 -0400

    added ShoppingList app

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+            System.out.println("Splenda");
+            System.out.println("Coffee");
+            System.out.println("Coke");
+            System.out.println("Eggs");
+            System.out.println("Cheese");
+            System.out.println("Tea");
+            System.out.println("Lettuce");
+            System.out.println("Apples");
+            System.out.println("Oranges");
+            System.out.println("Bananas");
+    }
+}
